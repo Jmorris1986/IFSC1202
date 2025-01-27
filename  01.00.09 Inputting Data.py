533ee18a5e500fbbc12bbc371de71026d3e60eff
Jmorris1986 <jmorris1@ualr.edu>
2025-01-27 16:41:36 +0000

    update

---
+++  01.00.09 Inputting Data.py
@@ -0,0 +1,5 @@
+firstname = input("Enteer Your Name")  # Input statement without a prompt
+print("You Entered: " + firstname)
+ 
+lastname = input("Enter Your Name:")  # Input statement with a prompt
+print("You Entered: " + lastname)
