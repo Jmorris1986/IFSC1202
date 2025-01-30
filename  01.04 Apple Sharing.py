5df6b46c3e2314630d12e13e7b3b3a12137f1883
Jmorris1986 <jmorris1@ualr.edu>
2025-01-30 08:00:00 +0000

    update

---
+++  01.04 Apple Sharing.py
@@ -0,0 +1,56 @@
+#N students take K apples from a basket and distribute them among each other evenly. The remaining (the undivisible) apples remains in the basket.
+
+# N is the number of students
+# K is the number of apples 
+
+N = 6
+K = 50
+
+#How many apples will each single student get?
+variable = (K / N)
+print(f"with zero decimal places: {variable:.0f}")
+
+# T is the total apples divided between the students
+T = 8
+
+#How many apples will remain in the basket?
+variable = (T * N)
+print(K - variable)
+
+#N students take K apples from a basket and distribute them among each other evenly. The remaining (the undivisible) apples remains in the basket.
+
+# N is the number of students
+# K is the number of apples 
+
+N = 5
+K = 25
+
+#How many apples will each single student get?
+variable = (K / N)
+print(f"with zero decimal places: {variable:.0f}")
+
+# T is the total apples divided between the students
+T = 5
+
+#How many apples will remain in the basket?
+variable = (T * N)
+print(K - variable)
+
+#N students take K apples from a basket and distribute them among each other evenly. The remaining (the undivisible) apples remains in the basket.
+
+# N is the number of students
+# K is the number of apples 
+
+N = 1
+K = 10
+
+#How many apples will each single student get?
+variable = (K / N)
+print(f"with zero decimal places: {variable:.0f}")
+
+# T is the total apples divided between the students
+T = 10
+
+#How many apples will remain in the basket?
+variable = (T * N)
+print(K - variable)
--- 01.02 Hello.py
+++ 01.02 Hello.py
@@ -1,1 +1,2 @@
-
+firstname = input("Enter Your Name")
+print("Hello" + firstname)
