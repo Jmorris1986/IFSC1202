204c10871d696582947b15800abd12bbb5297401
Jmorris1986 <jmorris1@ualr.edu>
2025-01-31 20:39:41 +0000

    update

---
+++ 2.00.00 integers and numbers.py
@@ -0,0 +1,42 @@
+print(17 / 3)       # gives 5.66666666667
+print(17 // 3)      # gives 5
+print(17 % 3)       # gives 2
+
+print(int(1.3))     # gives 1
+print(int(1.7))     # gives 1
+print(int(-1.3))    # gives -1
+print(int(-1.7))    # gives -1
+
+print(round(1.3))   # gives 1
+print(round(1.7))   # gives 2
+print(round(-1.3))  # gives -1
+print(round(-1.7))  # gives -2
+
+print(round(3.14159,2))   # gives 3.14
+print(round(3.14159,3))   # gives 3.142
+print(round(3.14159,4))   # gives 3.1416
+
+print(0.1 + 0.2)    # gives 0.30000000000000004
+
+from math import ceil 
+x = 7 / 2
+y = ceil(x)
+print(y)
+#Function Description
+
+#floor(x) = Return the floor of x, the largest integer less than or equal to x.
+
+#ceil(x) = Returns the ceiling of x, the smallest integer greater than or equal to x.
+
+#sqrt(x) = Returns the square root of x
+
+#log(x) = With one argument, return the natural logarithm of x (to base e). With two arguments, return the logarithm of x to the given base.
+
+#e = The mathematical constant e = 2,71828...
+
+#sin(x) = Return the sine of x radians
+
+#asin(x) = Return the arcsine of x, in radians
+
+#pi = The mathematical constant π = 3.1415...
+
