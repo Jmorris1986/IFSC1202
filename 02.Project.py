c89ca0db49616c37c55c9b89803251abddb91e3d
Jmorris1986 <jmorris1@ualr.edu>
2025-02-05 01:55:04 +0000

    update

--- 02.Project.py
+++ 02.Project.py
@@ -1,18 +1,11 @@
 import math
 
-
-
 def great_circle_distance(radius, lat1, lon1, lat2, lon2):
 
     lat1_rad = math.radians(lat1)
-
     lon1_rad = math.radians(lon1)
-
     lat2_rad = math.radians(lat2)
-
     lon2_rad = math.radians(lon2)
-
-
 
     distance = radius * math.acos(math.sin(lat1_rad) * math.sin(lat2_rad) + 
 
@@ -20,26 +13,13 @@ def great_circle_distance(radius, lat1, lon1, lat2, lon2):
 
                                   math.cos(lon2_rad - lon1_rad))
 
-
-
-    return round(distance, 2)  
-
-
-
-# Example usage:
+    return round(distance, 2)
 
 radius = 6371  
-
-start_lat = 52 
-
+start_lat = 52
 start_lon = 21
-
 end_lat = 41
-
 end_lon = 12
 
-
-
 distance = great_circle_distance(radius, start_lat, start_lon, end_lat, end_lon)
-
 print("Great Circle Distance:", distance, "km") 
